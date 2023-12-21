0cee794024fec5c97b15bf0bfefc864764ec3bc5
Mintttt <64011210@kmtil.ac.th>
2023-12-21 21:21:04 +0700

    ok

--- nodejs_test/process_argv.js
+++ nodejs_test/process_argv.js
@@ -1,7 +1,7 @@
 //เลือกคำนวณ พื้นที่สามเหลี่ยม หรือพื้นที่สี่เหลี่ยม
 
 //console.log(process.argv) //ใช้จัดการกับข้อมูลที่ส่งมาทาง command line เป็นในรูป array
-const minimist = require('minimist')
+const minimist = require('minimist') //เอาไว้ใส่หมวดหมู่ให้ค่า
 
 function get_triangle_area(width, height){
     return 1/2 * width * height
@@ -17,10 +17,11 @@ const clean_argument = minimist(argument) //แปลง argument เป็น o
 const {shape, w, h} = clean_argument //เปลี่ยน array เป็น object
 let area
 if (shape === 'tri'){
-    area = get_triangle_area(Number(w), Number(h))
+    area = get_triangle_area(w, h)
 }
 else if(shape === 'rec'){
-    area = get_rectangle_area(Number(w), Number(h))
+    area = get_rectangle_area(w, h))
 }
 console.log(argument)
+console.log(clean_argument)
 console.log(`area = ${area}`)
